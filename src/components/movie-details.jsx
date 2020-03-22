574228a53b9500e7e4adda63352eb2687b8c3be7
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-22 00:04:32 +0000

    Added FontAwesome link, rendered stars and conditionally added className with color depending on avg_rating value

--- src/components/movie-details.jsx
+++ src/components/movie-details.jsx
@@ -1,15 +1,42 @@
 import React, {Component} from "react";
 
+let FontAwesome = require('react-fontawesome');
+
 class MovieDetails extends Component {
     render() {
-        return(
+        const {movie} = this.props;
+
+        return (
             <React.Fragment>
                 {
                     // If the movie prop value isn't null, we display the details
-                    this.props.movie ? (
+                    movie ? (
                         <div>
-                            <h3>{this.props.movie.title}</h3>
-                            <p>{this.props.movie.description}</p>
+                            <h3>{movie.title}</h3>
+                            <p>
+                                <FontAwesome
+                                    name="star"
+                                    className={movie.avg_rating > 0 ? 'orange' : ''}
+                                />
+                                <FontAwesome
+                                    name="star"
+                                    className={movie.avg_rating > 1 ? 'orange' : ''}
+                                />
+                                <FontAwesome
+                                    name="star"
+                                    className={movie.avg_rating > 2 ? 'orange' : ''}
+                                />
+                                <FontAwesome
+                                    name="star"
+                                    className={movie.avg_rating > 3 ? 'orange' : ''}
+                                />
+                                <FontAwesome
+                                    name="star"
+                                    className={movie.avg_rating > 4 ? 'orange' : ''}
+                                />
+                                <span> ({movie.no_of_ratings})</span>
+                            </p>
+                            <p>{movie.description}</p>
                         </div>
                     ) : null
                 }
