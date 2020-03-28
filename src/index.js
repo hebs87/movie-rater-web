81f2812e4d7e73103ca794fe44bf887ad1ec4137
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-28 11:55:33 +0000

    Created basic Login component and created routes for Login and App components in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from "./components/login";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={Login}/>
+            <Route exact path="/movies" component={App}/>
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
