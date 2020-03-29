90a1b7e4dd714b570a3125607ebcc67a6c8ba733
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-29 11:29:50 +0100

    Amended Login component to only set the token if the user successfully logs in

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -27,13 +27,21 @@ class Login extends Component {
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(this.state.credentials)
         }).then(res => res.json())
-            // We use the cookies prop from the withCookies HOC and set it
-            // to the res.token - the first argument is the token name (can
-            // be whatever we want), and the second is the token from the
-            // res object. Then we redirect the user to the /movies route
+            // If the user successfully logs in, we use the cookies prop
+            // from the withCookies HOC and set it to the res.token - the
+            // first argument is the token name (can be whatever we want),
+            // and the second is the token from the res object. Then we
+            // redirect the user to the /movies route
+            // If the details don't match, we display an alert and redirect
+            // the user back to the login page
             .then(res => {
-                this.props.cookies.set('token', res.token);
-                window.location.href = '/movies'
+                if(res.token) {
+                    this.props.cookies.set('token', res.token);
+                    window.location.href = '/movies';
+                } else {
+                    window.location.href = '/';
+                    alert("Those details didn't match, please try again!")
+                }
             })
             .catch(error => console.log(error))
     };
