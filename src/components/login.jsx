a4ffe770b9fcd72cfc243d545395911a0d5d9b00
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-03-29 10:30:52 +0100

    Added functionality to get token on successful user login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,68 @@
 import React, {Component} from "react";
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            credentials: {
+                username: '',
+                password: ''
+            }
+        }
+    }
+
+    // A function to get the form's input field values
+    inputChanged = event => {
+        const credentials = this.state.credentials;
+        credentials[event.target.name] = event.target.value;
+        this.setState({credentials});
+    };
+
+    // A function to log the user in
+    login = event => {
+        fetch(`${process.env.REACT_APP_API_URL}/auth/`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(this.state.credentials)
+        }).then(res => res.json())
+            // We console log the token and then redirect the user
+            // to the /movies route
+            .then(res => {
+                console.log(res.token);
+                window.location.href = '/movies'
+            })
+            .catch(error => console.log(error))
+    };
+
     render() {
-        return <h1>Login</h1>
+        // Destructure the state
+        const {credentials} = this.state;
+
+        return (
+            <div className="login-container">
+                <h1>Login</h1>
+                <span>Username</span>
+                <br/>
+                <input
+                    type="text"
+                    name="username"
+                    value={credentials.username}
+                    onChange={this.inputChanged}
+                />
+                <br/>
+                <span>Password</span>
+                <br/>
+                <input
+                    type="password"
+                    name="password"
+                    value={credentials.password}
+                    onChange={this.inputChanged}
+                />
+                <br/>
+                <button onClick={this.login}>Login</button>
+            </div>
+        )
     }
 }
 
